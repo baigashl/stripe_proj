772ab266657ce42b9df47cc0d84afff336b9a8bd
Your Name <you@example.com>
2022-11-18 03:46:06 +0600

    initial

--- templates/checkout.js
+++ templates/checkout.js
@@ -8,7 +8,7 @@
     <p>1111</p>
     <button id="buy-button">Buy</button>
     <script type="text/javascript">
-      var stripe = Stripe('pk_test_51Lg6uJACJ0hmrHGCMQeL6WZhdGX1DFFneFU2Yt4CLeJ0VPb800om27v91uelCaO5bUPLKC6RYqmmVP1pPbxZHrAY00zoJ4OFbH  ');
+      var stripe = Stripe('  ');
       var buyButton = document.getElementById(buy-button');
       buyButton.addEventListener('click', function() {
         // Create a new Checkout Session using the server-side endpoint
